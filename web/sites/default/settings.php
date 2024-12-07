c3f6b8b128d8fe311a36160d25857a89c0a8da53
Mario Lasluisa <mjlasca@gmail.com>
2024-12-07 00:21:13 -0500

    configuration create content type and paragraph and taxonomy

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -258,7 +258,8 @@ $databases = [];
  * directory in the public files path. The setting below allows you to set
  * its location.
  */
-# $settings['config_sync_directory'] = '/directory/outside/webroot';
+$settings['config_sync_directory'] = '../config/sync';
+
 
 /**
  * Settings:
